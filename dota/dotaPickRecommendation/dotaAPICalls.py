52cc3902e877644237ed0b6fe6fad8d48d2785a7
berke <daw.berkeij@student.han.nl>
2025-11-18 14:14:35 +0100

    update api script

--- dota/dotaPickRecommendation/dotaAPICalls.py
+++ dota/dotaPickRecommendation/dotaAPICalls.py
@@ -1,19 +1,26 @@
 import requests
 import json
+import time
 
-url = "https://api.opendota.com/api/heroes"
-heroes = requests.get(url, timeout=60).json()
-hero_id_to_name = {hero['id']: hero['localized_name'] for hero in heroes}
-print(hero_id_to_name)
+def fetch_hero_data():
+    url = "https://api.opendota.com/api/heroes"
+    heroes = requests.get(url, timeout=100).json()
+    hero_id_to_name = {hero['id']: hero['localized_name'] for hero in heroes}
+    return hero_id_to_name
 
-url = "https://api.opendota.com/api/heroStats"
-heroes_stats = requests.get(url, timeout=60).json()
-hero_id_to_roles = {hero['id']: hero['roles'] for hero in heroes_stats}
-print(hero_id_to_roles)
+def fetch_hero_roles():
+    url = "https://api.opendota.com/api/heroStats"
+    heroes_stats = requests.get(url, timeout=100).json()
+    hero_id_to_roles = {hero['id']: hero['roles'] for hero in heroes_stats}
+    return hero_id_to_roles
 
-# save data to json files
-with open("hero_id_to_name.json", "w") as f:
-    json.dump(hero_id_to_name, f)
+def save_json(data, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
-with open("hero_id_to_roles.json", "w") as f:
-    json.dump(hero_id_to_roles, f)
+if __name__ == "__main__":
+    hero_id_to_name = fetch_hero_data()
+    save_json(hero_id_to_name, "hero_id_to_name.json")
+    time.sleep(1)
+    hero_id_to_roles = fetch_hero_roles()
+    save_json(hero_id_to_roles, "hero_id_to_roles.json")
